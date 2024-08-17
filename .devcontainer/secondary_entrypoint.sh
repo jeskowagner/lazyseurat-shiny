60483a407885a3e8f45d2bb51bfb6ad25b751068
Jesko Wagner <jesko.wagner@gmail.com>
2024-08-17 12:13:33 +0100

    fix port forwarding in dev container

--- .devcontainer/secondary_entrypoint.sh
+++ .devcontainer/secondary_entrypoint.sh
@@ -1,1 +1,1 @@
-Rscript -e 'shiny::runApp("/home/mambauser/app/", port=3838, launch.browser=FALSE)'
+Rscript -e 'shiny::runApp("/home/mambauser/app/", port=3838, host="0.0.0.0", launch.browser=FALSE)'
